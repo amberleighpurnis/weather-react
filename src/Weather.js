45e7219c50c495c66bf0265a4ef508b97a8772ac
Amber <amberleighpurnis@users.noreply.github.com>
2020-09-11 14:38:33 -0400

    added axios to show weather data

--- src/Weather.js
+++ src/Weather.js
@@ -3,4 +3,12 @@ import axios from "axios";
 
 export default function Weather() {
   return <h2>Hello from weather</h2>;
+
+  function fetchData(response) {
+    alert(`The weather for Toronto is ${response.data.main.temp}ºC`);
+  }
+  let key = `6140482334c764ca7fa9951280c40d98`;
+  let unit = "metric";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${key}&units=${unit}`;
+  axios.get(url).then(fetchData);
 }
