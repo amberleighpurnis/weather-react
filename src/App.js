20bb2af70baec3e54630216e13bb15a42e22b922
Amber <amberleighpurnis@users.noreply.github.com>
2020-09-11 18:01:17 -0400

    Added Footer and how to connect links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Weather from "./Weather";
 import Search from "./Search";
+import Footer from "./Footer";
 
 import "./App.css";
 import "./Weather.css";
 import "./Search.css";
+import "./Footer.css";
 
 function App() {
   return (
     <div className="App">
       <Search />
       <Weather />
+      <Footer />
     </div>
   );
 }
