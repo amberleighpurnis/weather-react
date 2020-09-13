0627c95c99eca585c019ee280844bd2696b3360b
Amber <amberleighpurnis@users.noreply.github.com>
2020-09-12 20:54:58 -0400

    Added Date and Sunrise/set Times

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
+import FormattedHours from "./FormattedHours";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -11,13 +13,13 @@ export default function Weather(props) {
       city: response.data.name,
       temperature: Math.round(response.data.main.temp),
       feelsLike: Math.round(response.data.main.feels_like),
-      date: response.data.dt,
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
       imgUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
-      sunrise: response.data.sys.sunrise,
-      sunset: response.data.sys.sunset,
+      sunrise: new Date(response.data.sys.sunrise * 1000),
+      sunset: new Date(response.data.sys.sunset * 1000),
     });
   }
   if (weatherData.ready) {
@@ -60,7 +62,9 @@ export default function Weather(props) {
         <div className="container-flex">
           <h1 className="cityName"> {weatherData.city} </h1>
           <ul className="city">
-            <li>Last updated: {weatherData.date}</li>
+            <li>
+              <FormattedDate date={weatherData.date} />
+            </li>
             <li>{weatherData.description}</li>
           </ul>
           <div className="row">
@@ -92,10 +96,13 @@ export default function Weather(props) {
                   <strong>Wind:</strong> {weatherData.wind} km/h
                 </p>
                 <p>
-                  <strong>Sunrise:</strong> {weatherData.sunrise}
+                  <FormattedHours
+                    time={`Sunrise:`}
+                    date={weatherData.sunrise}
+                  />
                 </p>
                 <p>
-                  <strong>Sunset:</strong> {weatherData.sunset}{" "}
+                  <FormattedHours time={`Sunset:`} date={weatherData.sunset} />{" "}
                 </p>
               </div>
             </div>
@@ -106,7 +113,6 @@ export default function Weather(props) {
   } else {
     const apiKey = `6140482334c764ca7fa9951280c40d98`;
     let unit = "metric";
-    let city = "Toronto";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${unit}`;
     axios.get(apiUrl).then(fetchData);
 
